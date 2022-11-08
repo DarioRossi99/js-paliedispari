44648458637ba1c4103da72561cc9c1b9b488788
DarioRossi99 <rssdra99@gmail.com>
2022-11-08 21:04:28 +0100

    inserimento codice pali

---
+++ js/pali.js
@@ -0,0 +1,19 @@
+let parola = prompt("inserire un parola singola");
+
+const parolaInversa = parola.split("").reverse().join("");
+console.log(parolaInversa);
+
+const parolaArrey = parola.split("");
+let parolaVersa
+
+for (let i = parolaArrey.length-1; i >= 0; i--) {
+    parolaVersa += parolaArrey;
+}
+console.log(parolaVersa);
+
+if (parola == parolaInversa){
+    console.log("la parola" + parola + "é palindroma");
+}else{
+    console.log(parolaArrey.reverse());
+    console.log("la parola" + parola + "NON è palindroma");
+}
