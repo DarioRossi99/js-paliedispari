f31654d00f5f633eb53933cf768243d1d8e3a0c3
DarioRossi99 <rssdra99@gmail.com>
2022-11-08 18:53:40 +0100

    aggiunta non valida

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
+const SezionePariDispariEl = document.querySelector(".PariDispari");
+const Btn = SezionePariDispariEl.querySelector("button");
 
-function getPariDispari(num){
-    if( number % 2 === 0){
+Btn.addEventListener("click", function(){
+    const inputEl = SezionePariDispariEl.querySelector('[name="numero"]');
+    const risultatoEl = document.querySelector(".risultato");
+
+    const risultato = getPariDispari(parseInt(inputEl.value));
+
+    risultatoEl.innerHTML = `Il numero ${inputEl.value} é ${risultato}`;
+})
+
+/** 
+ * @param {number} num
+ * @return {string}
+ */
+function getPariDispari (num){
+
+    if (isNaN( num )){
+        return "non valido";
+    }
+
+    if( num % 2 === 0){
         return "Pari";
     }else {
         return "Dispari";
     }
 }
+
+// 1 numero random dal pc 
+// 2 sommare i due numeri 
+// 3 far capire se la somma è P o D
+// 4 confrontare le 2 stringe pc e utente 
+// 5 creare if per il confronto 
+// 6 comunicare all utente se ha vinto o perso
